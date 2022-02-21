9b10d731860643922062c8522cd9b0b6cec6f598
JacobPacker <90899662+JacobPacker@users.noreply.github.com>
2022-02-21 16:09:06 +0000

    ore

--- Assets/Scripts/MusicControler.cs
+++ Assets/Scripts/MusicControler.cs
@@ -6,10 +6,18 @@ using UnityEngine.Audio;
 public class MusicControler : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    private bool isMuted;
+    void Start()
+    {
+        isMuted = false;
+    }
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("musicVol", volume);
-
     }
-
+    public void MutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+    }
 }
--- Assets/Scripts/MusicScript.cs
+++ Assets/Scripts/MusicScript.cs
@@ -7,18 +7,6 @@ public class MusicScript : MonoBehaviour
     [HideInInspector]
     public float musicVolume;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void SetMusicVolume(float inputValue)
     {
         musicVolume = inputValue;
